75449261ddd77be608471f24470a72f5b1acbdcb
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-05-23 23:57:27 +0530

    Upload memes from phone gallery to firebase storage

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,73 +1,85 @@
+var Platform = require('react-native').Platform;
+var ImagePicker = require('react-native-image-picker')
+import React from 'react';
 import {
     AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
   } from 'react-native';
+  import { Button, CardSection, Card } from './common'
   import firebase from 'firebase'
   import RNFetchBlob from 'react-native-fetch-blob'
-  import CameraRollPicker from 'react-native-camera-roll-picker'
-  export default class Gallery extends Component {
+
+  export default class ImageUpload extends React.Component {
     
-    getSelectedImages = (selectedImages, currentImage) => {
+    getSelectedImages() {
+      ImagePicker.showImagePicker((response) => {
+        console.log('Response', response);
+
+        if(response.didCancel) {
+          console.log('User cancelled');  
+        }
+        else if (response.error) {
+          console.log('Some error', response.error);
+        }
+        else if (response.customButton) {
+          console.log('customButton', response.customButton);
+        }else{
+          const image = response.uri
+
+
+          const Blob = RNFetchBlob.polyfill.Blob
+          const fs = RNFetchBlob.fs
+          window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+          window.Blob = Blob
       
-      const image = currentImage.uri
-   
-      const Blob = RNFetchBlob.polyfill.Blob
-      const fs = RNFetchBlob.fs
-      window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
-      window.Blob = Blob
-   
-     
-      let uploadBlob = null
-      const imageRef = firebase.storage().ref('posts').child("test.jpg")
-      let mime = 'image/jpg'
-      fs.readFile(image, 'base64')
-        .then((data) => {
-          return Blob.build(data, { type: `${mime};BASE64` })
-      })
-      .then((blob) => {
-          uploadBlob = blob
-          return imageRef.put(blob, { contentType: mime })
-        })
-        .then(() => {
-          uploadBlob.close()
-          return imageRef.getDownloadURL()
-        })
-        .then((url) => {
-          // URL of the image uploaded on Firebase storage
-          console.log(url);
-          
-        })
-        .catch((error) => {
-          console.log(error);
-   
-        })  
-   
+        
+          let uploadBlob = null
+          const imageRef = firebase.storage().ref('memes').child("test.jpg")
+          let mime = 'image/jpg'
+          fs.readFile(image, 'base64')
+            .then((data) => {
+              return Blob.build(data, { type: `${mime};BASE64` })
+          })
+          .then((blob) => {
+              uploadBlob = blob
+              return imageRef.put(blob, { contentType: mime })
+            })
+            .then(() => {
+              uploadBlob.close()
+              return imageRef.getDownloadURL()
+            })
+            .then((url) => {
+              // URL of the image uploaded on Firebase storage
+              console.log(url);
+              
+            })
+            .catch((error) => {
+              console.log(error);
+            })  
+        }
+      });
     }
+  
     render() {
    
       return (
-        <View style={styles.gallery}>
-          <CameraRollPicker selected={[]} maximum={1} callback={this.getSelectedImages} />
-          <Text style={styles.welcome}>
-            Image Gallery
-          </Text>        
-        </View>
+        <Card>
+          <CardSection>
+            <Button
+              text='Pick Image'
+              onPress={this.getSelectedImages.bind(this)} />
+          </CardSection>
+        </Card>
       );
     }
   }
    
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    gallery: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
+  const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
-  });
+};
