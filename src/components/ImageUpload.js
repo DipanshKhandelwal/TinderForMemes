4bb6b8496688165b5b4ebf2af7b5b500adf90319
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-05-24 22:42:34 +0530

    Add textview to display link to which last image was uploaded

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -12,6 +12,8 @@ import {
   import RNFetchBlob from 'react-native-fetch-blob'
 
   export default class ImageUpload extends React.Component {
+
+    state = {link: '' }
     
     getSelectedImages() {
       ImagePicker.showImagePicker((response) => {
@@ -26,7 +28,7 @@ import {
         else if (response.customButton) {
           console.log('customButton', response.customButton);
         }else{
-          const image = response.uri
+          const uploadUri = response.uri
 
 
           const Blob = RNFetchBlob.polyfill.Blob
@@ -35,8 +37,12 @@ import {
           window.Blob = Blob
       
         
+          const { currentUser } = firebase.auth();
+          const image = Platform.OS === 'ios' ? uploadUri.replace('file://', '') : uploadUri
+          const sessionId = new Date().getTime()
+          
           let uploadBlob = null
-          const imageRef = firebase.storage().ref('memes').child("test.jpg")
+          const imageRef = firebase.storage().ref(`/users/${ currentUser.uid }/memes`).child(`${sessionId}`)
           let mime = 'image/jpg'
           fs.readFile(image, 'base64')
             .then((data) => {
@@ -44,15 +50,19 @@ import {
           })
           .then((blob) => {
               uploadBlob = blob
+              console.log('started')
               return imageRef.put(blob, { contentType: mime })
             })
             .then(() => {
               uploadBlob.close()
+              console.log('ended')
               return imageRef.getDownloadURL()
             })
             .then((url) => {
               // URL of the image uploaded on Firebase storage
+              console.log('this is url');
               console.log(url);
+              this.setState({ link: url })
               
             })
             .catch((error) => {
@@ -66,6 +76,11 @@ import {
    
       return (
         <Card>
+          <CardSection>
+            <Text>
+              {this.state.link}
+            </Text>
+          </CardSection>
           <CardSection>
             <Button
               text='Pick Image'
